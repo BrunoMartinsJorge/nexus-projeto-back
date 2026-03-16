1d975cf2513b21980600b1b28dcfe5ace6ae5b8a
brunomartinsjorge <rexon300008@gmail.com>
2026-03-16 00:10:37 -0300

    refactor: Limpar módulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { PrismaModule } from './shared/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './shared/exceptions/HttpExceptionFilter';
+import { WalletModule } from './modules/wallet/wallet.module';
+import { WebhoockModule } from './modules/webhook/webhook.module';
+import { SwapModule } from './modules/swap/swap.module';
+import { MovimentacoesModule } from './modules/movimentacoes/movimentacoes.module';
+import { SaqueModule } from './modules/saque/saque.module';
+import { TransacoesModule } from './modules/transacoes/transacoes.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    WalletModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    WalletModule,
+    WebhoockModule,
+    SwapModule,
+    MovimentacoesModule,
+    SaqueModule,
+    TransacoesModule,
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
