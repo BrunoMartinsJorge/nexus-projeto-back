4786a0a37bfc3d0ca00b3d33a80c5c45023c3e13
brunomartinsjorge <rexon300008@gmail.com>
2026-03-16 14:41:43 -0300

    refactor: Adicionar URL do banco ao .env

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AuthMiddleware } from './core/middlewares/auth.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  console.log(process.env.URL_FRONT);
+  app.enableCors({
+    origin: [process.env.URL_FRONT],
+    credentials: true,
+  });
   const authMiddleware = new AuthMiddleware();
   app.use(authMiddleware.use.bind(authMiddleware));
   await app.listen(process.env.PORT ?? 3000);
